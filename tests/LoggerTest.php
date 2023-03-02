c5524eed5361d027bcfbdef27a3f9e5263855120
McDope <tobiasbaeumer@gmail.com>
2023-03-02 17:03:02 +0100

    Add test for Logger

---
+++ tests/LoggerTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Tests;
+
+use AmiDev\WrpDistributor\Logger;
+use Monolog\Handler\RotatingFileHandler;
+use Psr\Log\LoggerInterface;
+
+final class LoggerTest extends BaseTestCase {
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    public function testInfoLogsLikeExpected(): void
+    {
+        $monolog = $this->createMock(\Monolog\Logger::class);
+
+        $monolog
+            ->expects(self::once())
+            ->method('pushHandler');
+
+        $monolog
+            ->expects(self::once())
+            ->method('info')
+            ->with('test', ['elem1' => 'val1', 'elem2' => 'val2']);
+
+        $testLogger = new Logger($monolog);
+        $testLogger->info('test', ['elem1' => 'val1', 'elem2' => 'val2']);
+    }
+
+    public function testWarningLogsLikeExpected(): void
+    {
+        $monolog = $this->createMock(\Monolog\Logger::class);
+
+        $monolog
+            ->expects(self::once())
+            ->method('pushHandler');
+
+        $monolog
+            ->expects(self::once())
+            ->method('warning')
+            ->with('test', ['elem1' => 'val1', 'elem2' => 'val2']);
+
+        $testLogger = new Logger($monolog);
+        $testLogger->warning('test', ['elem1' => 'val1', 'elem2' => 'val2']);
+    }
+
+    public function testErrorLogsLikeExpected(): void
+    {
+        $monolog = $this->createMock(\Monolog\Logger::class);
+
+        $monolog
+            ->expects(self::once())
+            ->method('pushHandler');
+
+        $monolog
+            ->expects(self::once())
+            ->method('error')
+            ->with('test', ['elem1' => 'val1', 'elem2' => 'val2']);
+
+        $testLogger = new Logger($monolog);
+        $testLogger->error('test', ['elem1' => 'val1', 'elem2' => 'val2']);
+    }
+
+    public function testDebugLogsLikeExpected(): void
+    {
+        $monolog = $this->createMock(\Monolog\Logger::class);
+
+        $monolog
+            ->expects(self::once())
+            ->method('pushHandler');
+
+        $monolog
+            ->expects(self::once())
+            ->method('debug')
+            ->with('test', ['elem1' => 'val1', 'elem2' => 'val2']);
+
+        $testLogger = new Logger($monolog);
+        $testLogger->debug('test', ['elem1' => 'val1', 'elem2' => 'val2']);
+    }
+
+    public function testLogsEvenWithNonExistingLogDirectory(): void
+    {
+        $monolog = $this->createMock(\Monolog\Logger::class);
+
+        // Cheat "non-existing" log dir
+        $cwd = getcwd();
+        self::assertTrue(chdir('/tmp'));
+
+        $monolog
+            ->expects(self::once())
+            ->method('pushHandler');
+
+        $monolog
+            ->expects(self::once())
+            ->method('info')
+            ->with('test', ['elem1' => 'val1', 'elem2' => 'val2']);
+
+        $testLogger = new Logger($monolog);
+        $testLogger->info('test', ['elem1' => 'val1', 'elem2' => 'val2']);
+
+        self::assertTrue(chdir($cwd));
+    }
+}
