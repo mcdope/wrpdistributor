bf5e53485ba3b586e067bb27751639e3bc06a907
McDope <tobiasbaeumer@gmail.com>
2023-03-02 17:02:51 +0100

    Create BaseTestCase

---
+++ tests/BaseTestCase.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests;
+
+use AmiDev\WrpDistributor\Logger;
+use AmiDev\WrpDistributor\ServiceContainer;
+use Dotenv\Dotenv;
+use PHPUnit\Framework\TestCase;
+
+class BaseTestCase extends TestCase
+{
+    protected ServiceContainer $serviceContainer;
+
+    public function __construct(string $name)
+    {
+        parent::__construct($name);
+
+        $environmentVarsToLoad = [
+            'MAX_CONTAINERS_RUNNING',
+            'CONTAINER_HOSTS',
+            'CONTAINER_HOSTS_KEYS',
+            'CONTAINER_HOSTS_TLS_CERTS',
+            'CONTAINER_DISTRIBUTION_METHOD',
+            'SESSION_DATABASE_DSN',
+            'SESSION_DATABASE_USER',
+            'SESSION_DATABASE_PASS',
+            'START_PORT',
+            'AUTH_TOKEN'
+        ];
+
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../', ['.env.test']);
+        $dotenv->load();
+        $dotenv->required($environmentVarsToLoad);
+
+        $this->serviceContainer = new ServiceContainer(
+            new Logger(),
+            $this->createMock(\PDO::class),
+        );
+    }
+
+    public function tearDown(): void
+    {
+        unset($this->serviceContainer);
+    }
+}
+
+
